0943706fb50ff880a370eb68823feaef1e857b67
alanderekh <alanderekh@gmail.com>
2022-05-18 00:00:40 -0600

    Se ha agregado la galeria de imagenes

---
+++ app/src/main/java/com/example/ika/IMGalery.java
@@ -0,0 +1,19 @@
+package com.example.ika;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class IMGalery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imgalery);
+
+
+
+    }
+}
